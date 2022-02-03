05c70d712a63841d7949ded08202a94640958e19
Justin Swift <jlswift2@gmail.com>
2022-02-03 10:00:58 -0600

    commit tree bca83001dd349e4afc5419b1827d4f49df23bd60

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+  ]
+
+function superbowlWin(array) {
+    let helpMe = array.find( element =>  element.result === "W")
+    if (helpMe === undefined){
+        return undefined
+    } else {
+        return helpMe.year
+    }
+}
+
+
+  
+
